fba5189765223c8b04a3be4d68fffa84306175a2
18338739892 <656678879@qq.com>
2017-10-23 20:10:03 +0800

    更改项目结构

--- src/main/java/com/pkk/base/BaseAction.java
+++ src/main/java/com/pkk/base/BaseAction.java
@@ -1,12 +1,14 @@
 package com.pkk.base;
 
 import com.opensymphony.xwork2.ActionSupport;
+
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import org.apache.struts2.interceptor.SessionAware;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.util.Map;
 
 /**
@@ -19,7 +21,7 @@ import java.util.Map;
  */
 public class BaseAction extends ActionSupport implements ServletRequestAware, ServletResponseAware, SessionAware {
 
-    
+
     @Override
     public void setServletRequest(HttpServletRequest httpServletRequest) {
 
