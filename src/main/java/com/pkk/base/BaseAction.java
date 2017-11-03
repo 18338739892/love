a54770553414ffdcb3dd614f9b07488bd1e3a4c2
18338739892 <656678879@qq.com>
2017-11-03 10:37:29 +0800

    创建主页

--- src/main/java/com/pkk/base/BaseAction.java
+++ src/main/java/com/pkk/base/BaseAction.java
@@ -1,6 +1,7 @@
 package com.pkk.base;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.pkk.utils.condition.PageCondition;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
@@ -22,18 +23,87 @@ import java.util.Map;
 public class BaseAction extends ActionSupport implements ServletRequestAware, ServletResponseAware, SessionAware {
 
 
-    @Override
-    public void setServletRequest(HttpServletRequest httpServletRequest) {
+    public HttpServletRequest  servletRequest;// request对象
+    public HttpServletResponse servletResponse;// response对象
+    public int                 page;//第几页
+    public int                 rows;//每页显示的总条数
+    public String              sort;//排序字段
+    public String              order;//排序顺序
+    public PageCondition       pageCondition;//分页封装类
+    public Map                 session;//session对象
 
+
+    public HttpServletRequest getServletRequest() {
+        return servletRequest;
     }
 
     @Override
-    public void setServletResponse(HttpServletResponse httpServletResponse) {
+    public void setServletRequest(HttpServletRequest servletRequest) {
+        this.servletRequest = servletRequest;
+    }
 
+    public HttpServletResponse getServletResponse() {
+        return servletResponse;
     }
 
     @Override
-    public void setSession(Map<String, Object> map) {
+    public void setServletResponse(HttpServletResponse servletResponse) {
+        this.servletResponse = servletResponse;
+    }
 
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public void setPageCondition(PageCondition pageCondition) {
+        this.pageCondition = pageCondition;
+    }
+
+    public Map getSession() {
+        return session;
+    }
+
+    public void setSession(Map session) {
+        this.session = session;
+    }
+
+
+    public PageCondition getPageCondition() {
+        if (pageCondition == null) {
+            pageCondition = new PageCondition();
+        }
+        pageCondition.setPage(page);
+        pageCondition.setPagesize(rows);
+        pageCondition.setSortname(sort);
+        pageCondition.setSortorder(order);
+        return pageCondition;
     }
 }
---
+++ src/main/java/com/pkk/base/BaseService.java
@@ -0,0 +1,45 @@
+package com.pkk.base;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pkk.dao.impl.QueryUserDao;
+import com.pkk.model.UserLoginModel;
+
+/**
+ * @author peikunkun
+ * @version V1.0
+ * @Title: love
+ * @Package com.pkk.base
+ * @Description: <>
+ * @date 11/2 0002 13:27
+ */
+@Service
+public class BaseService<T> {
+
+    @Resource(name = "queryUserDao")
+    private QueryUserDao queryUserDao;
+
+    public QueryUserDao getQueryUserDao() {
+        return queryUserDao;
+    }
+
+    public void setQueryUserDao(QueryUserDao queryUserDao) {
+        this.queryUserDao = queryUserDao;
+    }
+
+    @Transactional
+    public UserLoginModel findUserInfo() {
+
+        System.out.println("dao信息为" + queryUserDao);
+
+        queryUserDao.findUserInFo();
+
+        return null;
+
+    }
+
+
+}
